49f1fabfec0a24cb4f8fde3d0257bc5438b8d7ee
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-12-18 18:07:43 +0100

    Function

--- index.php
+++ index.php
@@ -3,19 +3,29 @@ class Movie
 {
     public $title;
     public $genere;
+    public $voto;
 
-    function __construct($title, $genere)
+    function __construct($title, $genere, $voto)
     {
         $this->title = $title;
         $this->genere = $genere;
+        $this->voto = $voto;
     }
 
-    public function isAdult()
+    public function filmAprezzato()
     {
+        if ($this->voto >= 4) {
+            return 'Il film ha ottime valutazioni';
+        } else {
+            return 'Il film non è stato aprezzato dalla critica';
+        }
     }
 }
 
 
-$film1 = new Movie('Karate Kid', 'Azione');
+$film1 = new Movie('Karate Kid', 'Azione', 4);
 
 var_dump($film1);
+
+
+echo $film1->filmAprezzato();
